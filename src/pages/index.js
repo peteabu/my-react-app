8b422fed1630026122345bb75a17624e24d85d34
Peter Abu-Ekpeshie <peterabu-ekpeshie@Peters-MacBook-Pro.local>
2018-11-08 16:39:47 +0100

    my-react-app

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to Design and Code React Apps</h1>
+    <p>I just paid N5,505 naira for this, I hope I use it fully and enjoy the experience, the Nigga is hopefully driven.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
